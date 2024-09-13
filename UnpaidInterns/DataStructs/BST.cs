28fccdc317a07f4b2ab8588fdcba0eaf1548ea96
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-13 12:58:46 -0400

    bst add

--- UnpaidInterns/DataStructs/BST.cs
+++ UnpaidInterns/DataStructs/BST.cs
@@ -1,7 +1,7 @@
 class bstNode {
-    int val;
-    bstNode? left;
-    bstNode? right;
+    public int val;
+    public bstNode? left;
+    public bstNode? right;
 
     public bstNode(int x) {
         this.val = x;
@@ -11,5 +11,40 @@ class bstNode {
         this.val = x;
         this.left = l;
         this.right = r;
+    }
+}
+
+class bst {
+    bstNode root;
+
+    public bst(int x) {
+        this.root = new bstNode(x);
+    }
+
+    public bst(bstNode x) {
+        this.root = x;
+    }
+
+    public void add(int x) {
+        void check(int x, bstNode node) { // this solution is disgusting
+            if(x >= node.val ) {
+                if(node.right != null) {
+                    check(x, node.right);
+                } else {
+                    node.right = new bstNode(x);
+                    return;
+                }
+            }
+            if(x < node.val) {
+                if(node.left != null) {
+                    check(x, node.left);
+                } else {
+                    node.left = new bstNode(x);
+                    return;
+                }
+            }
+        }
+
+        check(x, this.root);
     }
 }
