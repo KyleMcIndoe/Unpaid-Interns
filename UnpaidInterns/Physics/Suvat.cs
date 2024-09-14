a8c0eedb9674b4e85004663395719e99f710547b
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-14 19:16:18 -0400

    Autofill suvat

--- UnpaidInterns/Physics/Suvat.cs
+++ UnpaidInterns/Physics/Suvat.cs
@@ -4,6 +4,8 @@
 // a = acceleration
 // t = time
 
+using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 public static class suvatFuncs {
@@ -43,7 +45,7 @@ public static class suvatFuncs {
         return v - (a * t);
     }
 
-    public static double uwihtoutv(double[] ins) {
+    public static double uwithoutv(double[] ins) {
         double s = ins[0];
         double a = ins[3];
         double t = ins[4];
@@ -165,6 +167,62 @@ public static class suvatFuncs {
 
     public static double[] findUnknowns(double?[] inputs) { // take an array [s, u, v, a, t] and find missing values
         double[] ans = new double[5];
+        string[] suvatChars = {"s", "u", "v", "a", "t"};
+        double[] funcInputs = new double[5];
+
+        List<int> unknownIndices = new List<int>();
+
+        string unkone = "";
+        string unktwo = "";
+
+        for(int i = 0; i < inputs.Length; i++) {
+            if(inputs[i] == null) {
+                unknownIndices.Add(i);
+                funcInputs[i] = 0;
+            } else {
+                #pragma warning disable CS8629 // Nullable value type may be null. I just wanted vscode to shut up, its wrong
+                ans[i] = (double)inputs[i];
+                funcInputs[i] = (double)inputs[i];
+                #pragma warning restore CS8629 // Nullable value type may be null.
+            }
+        }
+
+        if(unknownIndices.Count > 2) {
+            throw new Exception("Not enough data, need 3 known values");
+        }
+
+        unkone = suvatChars[unknownIndices[0]];
+        unktwo = suvatChars[unknownIndices[1]];
+
+        double callFromString(string s) { // This is coding GORE, but i dont want to use reflection, so its ok
+            if(s == "swithoutu") return swithoutu(funcInputs);
+            if(s == "swithoutv") return swithoutv(funcInputs);
+            if(s == "swithouta") return swithouta(funcInputs);
+            if(s == "swithoutt") return swithoutt(funcInputs);
+            if(s == "uwithouts") return uwithouts(funcInputs);
+            if(s == "uwithoutv") return uwithoutv(funcInputs);
+            if(s == "uwithouta") return uwithouta(funcInputs);
+            if(s == "uwithoutt") return uwithoutt(funcInputs);
+            if(s == "vwithouts") return vwithouts(funcInputs);
+            if(s == "vwithoutu") return vwithoutu(funcInputs);
+            if(s == "vwithouta") return vwithouta(funcInputs);
+            if(s == "vwithoutt") return vwithoutt(funcInputs);
+            if(s == "awithouts") return awithouts(funcInputs);
+            if(s == "awithoutu") return awithoutu(funcInputs);
+            if(s == "awithoutv") return awithoutv(funcInputs);
+            if(s == "awithoutt") return awithoutt(funcInputs);
+            if(s == "twithouts") return twithouts(funcInputs);
+            if(s == "twithoutu") return twithoutu(funcInputs);
+            if(s == "twithoutv") return twithoutv(funcInputs);
+            if(s == "twithouta") return twithouta(funcInputs);
+            return 0;
+        }
+
+        double unkoneans = callFromString(unkone + "without" + unktwo);
+        double unktwoans = callFromString(unktwo + "without" + unkone);
+
+        ans[unknownIndices[0]] = unkoneans;
+        ans[unknownIndices[1]] = unktwoans;
 
         return ans;
     }
