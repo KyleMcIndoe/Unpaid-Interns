5e77b0275cebcb4d5e4a23f7baff04382bc573f9
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-14 16:50:04 -0400

    Stack data struct

--- UnpaidInterns/DataStructs/Stack.cs
+++ UnpaidInterns/DataStructs/Stack.cs
@@ -1,3 +1,21 @@
 public class stack {
-    
+    List<int> elements = new List<int>();
+
+    public stack() {
+
+    }
+
+    public void push(int x) {
+        elements.Add(x);
+    }
+
+    public int pop() {
+        int x = elements[elements.Count - 1];
+        elements.RemoveAt(elements.Count - 1);
+        return x;
+    }
+
+    public int count() {
+        return elements.Count;
+    }
 }
