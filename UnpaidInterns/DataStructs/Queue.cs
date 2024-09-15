2e8bdbd2ca81cabdecab44d0488922ea1d3d6b40
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-15 16:54:04 -0400

    Queue + queue generics

--- UnpaidInterns/DataStructs/Queue.cs
+++ UnpaidInterns/DataStructs/Queue.cs
@@ -1,3 +1,29 @@
-class queue<t> {
-    List<t> elements = new List<t>();
+class queue<T> {
+    List<T> elements = new List<T>();
+
+    public queue() {
+
+    }
+
+    public void push(T x) {
+        elements.Add(x);
+    }
+
+    public int count() {
+        return elements.Count;
+    }
+
+    public T front() {
+        return elements[0];
+    }
+
+    public T back() {
+        return elements[elements.Count - 1];
+    }
+
+    public T pop() {
+        T x = elements[0];
+        elements.RemoveAt(0);
+        return x;
+    }
 }
