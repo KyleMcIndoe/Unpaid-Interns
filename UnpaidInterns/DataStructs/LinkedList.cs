d0689cdcc93d3dd7180ba38b8e70b11616dc76a9
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-15 16:48:19 -0400

    Linked list generics

--- UnpaidInterns/DataStructs/LinkedList.cs
+++ UnpaidInterns/DataStructs/LinkedList.cs
@@ -1,36 +1,36 @@
-public class listNode {
-    public int val;
-    public listNode? next;
+public class listNode<T> {
+    public T val;
+    public listNode<T>? next;
 
-    public listNode(int x) {
+    public listNode(T x) {
         this.val = x;
         this.next = null;
     }
 
-    public listNode(int x, listNode n) {
+    public listNode(T x, listNode<T> n) {
         this.val = x;
         this.next = n;
     }
 }
 
-public class linkedList {
-    public listNode head;
+public class linkedList<T> {
+    public listNode<T> head;
 
-    public linkedList(int x) {
-        this.head = new listNode(x);
+    public linkedList(T x) {
+        this.head = new listNode<T>(x);
     }
 
-    public int? getVal(int index) {
-        listNode? n = this.head;
+    public T? getVal(int index) {
+        listNode<T>? n = this.head;
         for(int i = 0; i <= index && n != null; i++) {
             if(i == index) return n.val;
             n = n.next;
         }
-        return null;
+        return default(T);
     }
 
-    public listNode? getNode(int index) {
-        listNode? n = this.head;
+    public listNode<T>? getNode(int index) {
+        listNode<T>? n = this.head;
         for(int i = 0; i <= index && n != null; i++) {
             if(i == index) return n;
             n = n.next;
@@ -40,33 +40,33 @@ public class linkedList {
 
     public int count() {
         int l = 0;
-        listNode? n = this.head;
+        listNode<T>? n = this.head;
         while(n != null) {
             l++;
             n = n.next;
         }
         return l;
     }
-    public listNode getLast() {
-        listNode n = this.head;
+    public listNode<T> getLast() {
+        listNode<T> n = this.head;
         while(n.next != null)  n = n.next;
         return n;
     }
-    public void add(int x) {
-        getLast().next = new listNode(x);
+    public void add(T x) {
+        getLast().next = new listNode<T>(x);
     }
 
-    public void add(int x, int index) {
+    public void add(T x, int index) {
         if(index >= this.count()) {
             this.add(x);
             return;
         }
 
-        listNode? n = this.head;
+        listNode<T>? n = this.head;
         for(int i = 0; i < index && n.next != null; i++) {
             if(i == index - 1) {
-                listNode a = n.next;
-                n.next = new listNode(x, a);
+                listNode<T> a = n.next;
+                n.next = new listNode<T>(x, a);
             }
             n = n.next;
         }
@@ -74,23 +74,23 @@ public class linkedList {
 
     public void remove(int index) {
 
-        listNode? n = this.head;
+        listNode<T>? n = this.head;
         for(int i = 0; i <= index && n != null; i++) {
             if(i == index - 1 && n.next != null) {
                 if(n.next.next == null) {
                     n.next = null;
                     return;
                 }
-                listNode toR = n.next;
+                listNode<T> toR = n.next;
                 n.next = toR.next;
                 toR.next = null;
             }
         }
     }
 
-    public int[] toArray() {
-        int[] ans = new int[this.count()];
-        listNode? n = this.head;
+    public T[] toArray() {
+        T[] ans = new T[this.count()];
+        listNode<T>? n = this.head;
 
         for(int i = 0; n != null; i++) {
             ans[i] = n.val;
