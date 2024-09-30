bca875a2550b81c84d0b584adb269b1e796c399d
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-30 10:15:40 -0400

    Constants

---
+++ UnpaidInterns/Math/Constants.cs
@@ -0,0 +1,6 @@
+public static class constants {
+    public static double pi = 3.14159265358978;
+    public static double e, eulers = 2.718281828459045;
+    public static double g, gravity = 9.81;
+
+}
