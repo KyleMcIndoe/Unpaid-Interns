895aee289e09d856ff9f74720178a38298cab995
KyleMcIndoe <kmcindoe06@gmail.com>
2024-10-04 12:06:30 -0400

    Polynomial term

--- UnpaidInterns/Math/Polynomials.cs
+++ UnpaidInterns/Math/Polynomials.cs
@@ -1,3 +1,13 @@
 public static class polyMath {
-    
+    class term {
+        char variable;
+        int coefficient;
+        int exponent;
+
+        public term(char v, int c, int e) {
+            this.variable = v;
+            this.coefficient = c;
+            this.exponent = e;
+        }
+    }
 }
