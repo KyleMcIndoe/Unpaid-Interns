e7672f519bc02fee4c1366f0f9a773c380fee717
KyleMcIndoe <kmcindoe06@gmail.com>
2024-10-04 12:12:46 -0400

    Polynomial derivatives

--- UnpaidInterns/Math/Polynomials.cs
+++ UnpaidInterns/Math/Polynomials.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 public static class polyMath {
     class term {
         char variable;
@@ -8,6 +10,21 @@ public static class polyMath {
             this.variable = v;
             this.coefficient = c;
             this.exponent = e;
+        }
+
+        void derivative() {
+            this.coefficient *= this.exponent;
+            this.exponent -= 1;
+        }
+    }
+
+    class polyNomial {
+        List<term> terms = new List<term>();
+
+        public polyNomial(List<term> ins) {
+            foreach(term x in ins) {
+                terms.Add(x);
+            }
         }
     }
 }
